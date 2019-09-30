4db446a5125f3576bbf28ec0ab9eecc6e7e3c114
Domen Kožar <domen@dev.si>
2019-09-30 17:58:28 +0200

    Install Nix via the action

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,47 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as tc from '@actions/tool-cache';
 import {homedir} from 'os';
+import {existsSync} from 'fs';
 import {extrasperse} from './utils';
 
 async function run() {
   try {
+    // inputs
     const file = core.getInput('file');
     const attributes = core.getInput('attributes');
     const cachixPush = core.getInput('cachixPush', { required: true });
+    const signingKey = core.getInput('signingKey', { required: true });
 
-    // required for macos
-    core.exportVariable('NIX_SSL_CERT_FILE', '/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt');
+    // rest of the constants
+    const home = homedir();
+    const PATH = process.env.PATH;  
+    const CERTS_PATH = home + '/.nix-profile/etc/ssl/certs/ca-bundle.crt';
 
-    console.log(`Installing Cachix ...`);
-    await exec.exec(homedir() + '/.nix-profile/bin/nix-env', ['-iA', 'cachix', '-f', 'https://cachix.org/api/v1/install']);
+    core.startGroup('Installing Nix')
+    // TODO: retry due to all the things that go wrong
+    const nixInstall = await tc.downloadTool('https://nixos.org/nix/install');
+    await exec.exec("sh", [nixInstall]);
+    core.exportVariable('PATH', `${PATH}:${home}/.nix-profile/bin`)
+    core.endGroup()
 
+    // macOS needs certificates hints
+    if (existsSync(CERTS_PATH)) {
+      core.exportVariable('NIX_SSL_CERT_FILE', CERTS_PATH);
+    }
+
+    core.startGroup('Installing Cachix')
+    await exec.exec('nix-env', ['-iA', 'cachix', '-f', 'https://cachix.org/api/v1/install']);
+    core.endGroup()
+
+    core.startGroup(`Using Cachix ` + cachixPush);
+    await exec.exec('cachix', ['use', cachixPush]);
+    core.endGroup()
+
+    core.exportVariable('CACHIX_SIGNING_KEY', signingKey)
     // TODO: cachix use --watch-store
 
-    console.log(`Setting up cache ` + cachixPush + `...`);
-    await exec.exec(homedir() + '/.nix-profile/bin/cachix', ['use', cachixPush]);
-
-    console.log(`Invoking nix-build...`);
+    core.startGroup(`Invoking nix-build`);
     let paths = '';
     const options = {
       listeners: {
@@ -29,13 +50,15 @@ async function run() {
         },
       }
     };
-    const args = ['-f', file || "default.nix"].concat(extrasperse('-A', attributes.split(/\s/)));
-    await exec.exec(homedir() + '/.nix-profile/bin/nix-build', args, options);
+    const args = extrasperse('-A', attributes.split(/\s/)).concat([file || "default.nix"]);
+    await exec.exec('nix-build', args, options);
+    core.endGroup()
 
-    console.log(`Pushing to cache ` + cachixPush + `...`);
-    await exec.exec(homedir() + '/.nix-profile/bin/cachix', ['push', cachixPush].concat(paths.split(/\s/).join(' ')));
+    core.startGroup(`Pushing to Cachix ` + cachixPush);
+    await exec.exec('cachix', ['push', cachixPush].concat(paths.split(/\s/).join(' ')));
+    core.endGroup()
   } catch (error) {
-    core.setFailed(`Action faield with error: ${error}`);
+    core.setFailed(`Action failed with error: ${error}`);
     throw(error);
   } 
 }
