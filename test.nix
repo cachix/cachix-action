e1972a3e880679c01e7ecd4d3c8ec5b41f08350d
Domen Kožar <domen@dev.si>
2019-10-03 15:33:27 +0200

    fix for #3

--- test.nix
+++ test.nix
@@ -10,6 +10,4 @@ let
   drv = i: runCommand "${toString currentTime}-${toString i}" {} ''
     dd if=/dev/zero of=$out bs=${toString size}MB count=1
   '';
-in writeText "empty-${toString num}-${toString size}MB" ''
-  ${lib.concatMapStringsSep "" drv (lib.range 1 num)}
-''
+in lib.listToAttrs (builtins.map (i: lib.nameValuePair "drv${toString i}" (drv i)) (lib.range 1 num))
