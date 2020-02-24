b96c355e8e59214ad0bd60110a4fe22b2a94c557
Domen Kožar <domen@dev.si>
2020-02-24 14:14:49 +0100

    macos: fix transient error
    if the daemon is not accesible via the socket, it client can fallback
    and fail with mysterious error.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import { type } from 'os';
+import { createConnection } from 'net';
 import { prependEach, nonEmptySplit } from './strings';
+import { exit } from 'process';
 
 function home() {
   if (type() == "Darwin") {
@@ -14,10 +16,6 @@ function home() {
 async function run() {
   try {
     // inputs
-    const file = core.getInput('file');
-    const skipNixBuild = core.getInput('skipNixBuild');
-    const attributes = core.getInput('attributes');
-    const nixBuildArgs = core.getInput('nixBuildArgs');
     const name = core.getInput('name', { required: true });
     const signingKey = core.getInput('signingKey');
     const authToken = core.getInput('authToken')
@@ -58,27 +56,49 @@ async function run() {
       } else {
         await exec.exec("sudo", ["pkill", "-HUP", "nix-daemon"], options);
       }
-
       core.endGroup();
-    }
-    if (skipNixBuild !== 'true') {
-      core.startGroup(`Invoking nix-build`);
-      let paths = '';
-      const options = {
-        listeners: {
-          stdout: (data: Buffer) => {
-            paths += data.toString();
-          },
-        }
-      };
-      const args = prependEach('-A', nonEmptySplit(attributes, /\s+/)).concat([file || "default.nix"]);
-      const additionalArgs = nonEmptySplit(nixBuildArgs, /\s+/);
-      await exec.exec('nix-build', additionalArgs.concat(args), options);
-      core.endGroup()
+
+      await awaitSocket();
     }
   } catch (error) {
     core.setFailed(`Action failed with error: ${error}`);
     throw (error);
+  }
+}
+
+async function awaitSocket() {
+  const daemonSocket = createConnection({ path: '/nix/var/nix/daemon-socket/socket' });
+  daemonSocket.on('error', async () => {
+    console.log('Waiting for daemon socket to be available, reconnecting...');
+    await new Promise(resolve => setTimeout(resolve, 500));
+    await awaitSocket();
+  });
+  daemonSocket.on('connect', async () => {
+    await build();
+  });
+}
+
+async function build() {
+  const file = core.getInput('file');
+  const skipNixBuild = core.getInput('skipNixBuild');
+  const attributes = core.getInput('attributes');
+  const nixBuildArgs = core.getInput('nixBuildArgs');
+
+  if (skipNixBuild !== 'true') {
+    core.startGroup(`Invoking nix-build`);
+    let paths = '';
+    const options = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          paths += data.toString();
+        },
+      }
+    };
+    const args = prependEach('-A', nonEmptySplit(attributes, /\s+/)).concat([file || "default.nix"]);
+    const additionalArgs = nonEmptySplit(nixBuildArgs, /\s+/);
+    await exec.exec('nix-build', additionalArgs.concat(args), options);
+    core.endGroup()
+    exit(0);
   }
 }
 
