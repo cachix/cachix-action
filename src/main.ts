7bea2cc0c144bc15e01cbd5b2e419c08bd26223b
Domen Kožar <domen@dev.si>
2020-02-24 14:14:49 +0100

    Simplify how paths are pushed
    Previously we were setting up Nix build hook to make sure all paths are
    synced to cachix.
    
    This had a few flaws:
    
    - nix-daemon had to be restarted, resulting into a few seconds extra
      setup per each build
    - spurious error on macos due to nix-daemon socket not ready in time
    - significant overhead since syncing to cachix was synchronous for each
      derivation built
    
    Now we just look for new store paths and push those after nix-build.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
-import { type } from 'os';
 import { prependEach, nonEmptySplit } from './strings';
+import { exit } from 'process';
 
-function home() {
-  if (type() == "Darwin") {
-    return "/Users/runner";
-  } else {
-    return "/home/runner";
-  }
-}
 
 async function run() {
   try {
     // inputs
+    const name = core.getInput('name', { required: true });
     const file = core.getInput('file');
     const skipNixBuild = core.getInput('skipNixBuild');
     const attributes = core.getInput('attributes');
     const nixBuildArgs = core.getInput('nixBuildArgs');
-    const name = core.getInput('name', { required: true });
     const signingKey = core.getInput('signingKey');
     const authToken = core.getInput('authToken')
     const cachixExecutable = "/nix/var/nix/profiles/per-user/runner/profile/bin/cachix";
@@ -37,44 +30,31 @@ async function run() {
     core.endGroup();
 
     if (signingKey !== "") {
-      core.startGroup('Cachix: Configuring push');
-      // needed to discover auth token
-      await exec.exec("sudo", ["sh", "-c", `echo export HOME=${home()} > /etc/nix/cachix-push.sh`]);
-      await exec.exec("sudo", ["sh", "-c", `echo export CACHIX_SIGNING_KEY=${signingKey} >> /etc/nix/cachix-push.sh`]);
-      // needed to for nix-store
-      await exec.exec("sudo", ["sh", "-c", `echo export PATH=\\$PATH:/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/per-user/runner/profile/bin >> /etc/nix/cachix-push.sh`]);
-      await exec.exec("sudo", ["sh", "-c", `echo ${cachixExecutable} push ${name} \\$OUT_PATHS >> /etc/nix/cachix-push.sh`]);
-      await exec.exec("sudo", ["sh", "-c", `chmod +x /etc/nix/cachix-push.sh`]);
-      // enable post-build-hook
-      await exec.exec("sudo", ["sh", "-c", `echo post-build-hook = /etc/nix/cachix-push.sh >> /etc/nix/nix.conf`]);
       core.exportVariable('CACHIX_SIGNING_KEY', signingKey);
+   
+      if (skipNixBuild !== 'true') {
+        core.startGroup(`Invoking nix-build`);
 
-      // Ignore reloading failures as Nix might be installed in single-user mode (install-nix-action version 5 or lower)
-      const options = { ignoreReturnCode: true };
-      // Reload nix-daemon
-      if (type() == "Darwin") {
-        // kickstart awaits nix-daemon to get up again
-        await exec.exec("sudo", ["launchctl", "kickstart", "-k", "system/org.nixos.nix-daemon"], options);
-      } else {
-        await exec.exec("sudo", ["pkill", "-HUP", "nix-daemon"], options);
+        // Remember existing store paths
+        await exec.exec("sh", ["-c", `nix path-info --all | grep -v '\.drv$' > store-path-pre-build`]);
+
+        let paths = '';
+        const options = {
+          listeners: {
+            stdout: (data: Buffer) => {
+              paths += data.toString();
+            },
+          }
+        };
+        const args = prependEach('-A', nonEmptySplit(attributes, /\s+/)).concat([file || "default.nix"]);
+        const additionalArgs = nonEmptySplit(nixBuildArgs, /\s+/);
+        await exec.exec('nix-build', additionalArgs.concat(args), options);
+        core.endGroup()
+
+        core.startGroup('Cachix: Pushing paths');
+        await exec.exec("sh", ["-c", `nix path-info --all | grep -v '\.drv$' | cat - store-path-pre-build | sort | uniq -u  | ${cachixExecutable} push ${name}`]);
+        core.endGroup();
       }
-
-      core.endGroup();
-    }
-    if (skipNixBuild !== 'true') {
-      core.startGroup(`Invoking nix-build`);
-      let paths = '';
-      const options = {
-        listeners: {
-          stdout: (data: Buffer) => {
-            paths += data.toString();
-          },
-        }
-      };
-      const args = prependEach('-A', nonEmptySplit(attributes, /\s+/)).concat([file || "default.nix"]);
-      const additionalArgs = nonEmptySplit(nixBuildArgs, /\s+/);
-      await exec.exec('nix-build', additionalArgs.concat(args), options);
-      core.endGroup()
     }
   } catch (error) {
     core.setFailed(`Action failed with error: ${error}`);
