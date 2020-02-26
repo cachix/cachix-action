8fa8dbc03809bd6b9cb79c2ecebd80b7bf3a154f
Domen Kožar <domen@dev.si>
2020-02-26 17:45:15 +0100

    simplify

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
-import { type } from 'os';
-import { createConnection } from 'net';
 import { prependEach, nonEmptySplit } from './strings';
 import { exit } from 'process';
 
-function home() {
-  if (type() == "Darwin") {
-    return "/Users/runner";
-  } else {
-    return "/home/runner";
-  }
-}
 
 async function run() {
   try {
@@ -36,46 +27,19 @@ async function run() {
 
     if (signingKey !== "") {
       core.startGroup('Cachix: Configuring push');
-      // needed to discover auth token
-      await exec.exec("sudo", ["sh", "-c", `echo export HOME=${home()} > /etc/nix/cachix-push.sh`]);
-      await exec.exec("sudo", ["sh", "-c", `echo export CACHIX_SIGNING_KEY=${signingKey} >> /etc/nix/cachix-push.sh`]);
-      // needed to for nix-store
-      await exec.exec("sudo", ["sh", "-c", `echo export PATH=\\$PATH:/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/per-user/runner/profile/bin >> /etc/nix/cachix-push.sh`]);
-      await exec.exec("sudo", ["sh", "-c", `echo ${cachixExecutable} push ${name} \\$OUT_PATHS >> /etc/nix/cachix-push.sh`]);
-      await exec.exec("sudo", ["sh", "-c", `chmod +x /etc/nix/cachix-push.sh`]);
-      // enable post-build-hook
-      await exec.exec("sudo", ["sh", "-c", `echo post-build-hook = /etc/nix/cachix-push.sh >> /etc/nix/nix.conf`]);
       core.exportVariable('CACHIX_SIGNING_KEY', signingKey);
-
-      // Ignore reloading failures as Nix might be installed in single-user mode (install-nix-action version 5 or lower)
-      const options = { ignoreReturnCode: true };
-      // Reload nix-daemon
-      if (type() == "Darwin") {
-        // kickstart awaits nix-daemon to get up again
-        await exec.exec("sudo", ["launchctl", "kickstart", "-k", "system/org.nixos.nix-daemon"], options);
-      } else {
-        await exec.exec("sudo", ["pkill", "-HUP", "nix-daemon"], options);
-      }
       core.endGroup();
 
-      await awaitSocket();
+      await build();
+
+      core.startGroup('Cachix: Pushing paths');
+      await exec.exec("sh", ["-c", `nix path-info --all | grep -v '\.drv$' | ${cachixExecutable} push ${name}`]);
+      core.endGroup();
     }
   } catch (error) {
     core.setFailed(`Action failed with error: ${error}`);
     throw (error);
   }
-}
-
-async function awaitSocket() {
-  const daemonSocket = createConnection({ path: '/nix/var/nix/daemon-socket/socket' });
-  daemonSocket.on('error', async () => {
-    console.log('Waiting for daemon socket to be available, reconnecting...');
-    await new Promise(resolve => setTimeout(resolve, 500));
-    await awaitSocket();
-  });
-  daemonSocket.on('connect', async () => {
-    await build();
-  });
 }
 
 async function build() {
@@ -98,7 +62,6 @@ async function build() {
     const additionalArgs = nonEmptySplit(nixBuildArgs, /\s+/);
     await exec.exec('nix-build', additionalArgs.concat(args), options);
     core.endGroup()
-    exit(0);
   }
 }
 
