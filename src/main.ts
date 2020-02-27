7ec3f9301e268510f676c00221f4084e4ed67e27
Domen Kožar <domen@dev.si>
2020-02-27 14:58:49 +0100

    push only built paths

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function run() {
   try {
     // inputs
     const name = core.getInput('name', { required: true });
+    const file = core.getInput('file');
+    const skipNixBuild = core.getInput('skipNixBuild');
+    const attributes = core.getInput('attributes');
+    const nixBuildArgs = core.getInput('nixBuildArgs');
     const signingKey = core.getInput('signingKey');
     const authToken = core.getInput('authToken')
     const cachixExecutable = "/nix/var/nix/profiles/per-user/runner/profile/bin/cachix";
@@ -26,42 +30,35 @@ async function run() {
     core.endGroup();
 
     if (signingKey !== "") {
-      core.startGroup('Cachix: Configuring push');
       core.exportVariable('CACHIX_SIGNING_KEY', signingKey);
-      core.endGroup();
+   
+      if (skipNixBuild !== 'true') {
+        core.startGroup(`Invoking nix-build`);
 
-      await build();
+        // Remember existing store paths
+        await exec.exec("sh", ["-c", `nix path-info --all | grep -v '\.drv$' > store-path-pre-build`]);
 
-      core.startGroup('Cachix: Pushing paths');
-      await exec.exec("sh", ["-c", `nix path-info --all | grep -v '\.drv$' | ${cachixExecutable} push ${name}`]);
-      core.endGroup();
+        let paths = '';
+        const options = {
+          listeners: {
+            stdout: (data: Buffer) => {
+              paths += data.toString();
+            },
+          }
+        };
+        const args = prependEach('-A', nonEmptySplit(attributes, /\s+/)).concat([file || "default.nix"]);
+        const additionalArgs = nonEmptySplit(nixBuildArgs, /\s+/);
+        await exec.exec('nix-build', additionalArgs.concat(args), options);
+        core.endGroup()
+
+        core.startGroup('Cachix: Pushing paths');
+        await exec.exec("sh", ["-c", `nix path-info --all | grep -v '\.drv$' | cat - store-path-pre-build | sort | uniq -u  | ${cachixExecutable} push ${name}`]);
+        core.endGroup();
+      }
     }
   } catch (error) {
     core.setFailed(`Action failed with error: ${error}`);
     throw (error);
-  }
-}
-
-async function build() {
-  const file = core.getInput('file');
-  const skipNixBuild = core.getInput('skipNixBuild');
-  const attributes = core.getInput('attributes');
-  const nixBuildArgs = core.getInput('nixBuildArgs');
-
-  if (skipNixBuild !== 'true') {
-    core.startGroup(`Invoking nix-build`);
-    let paths = '';
-    const options = {
-      listeners: {
-        stdout: (data: Buffer) => {
-          paths += data.toString();
-        },
-      }
-    };
-    const args = prependEach('-A', nonEmptySplit(attributes, /\s+/)).concat([file || "default.nix"]);
-    const additionalArgs = nonEmptySplit(nixBuildArgs, /\s+/);
-    await exec.exec('nix-build', additionalArgs.concat(args), options);
-    core.endGroup()
   }
 }
 
