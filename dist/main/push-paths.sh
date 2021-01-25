9831d0f47e3eeacd123467e4384038e3b810493e
zimbatm <zimbatm@zimbatm.com>
2021-01-25 10:39:33 +0100

    nix: only use stable commands
    Avoid the following error when installing nix-unstable:
    
        experimental Nix feature 'nix-command' is disabled; use '--experimental-features nix-command' to override

--- dist/main/push-paths.sh
+++ dist/main/push-paths.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-comm -13 <(sort /tmp/store-path-pre-build) <(nix path-info --all | grep -v '\.drv$' | sort) | $1 push -j8 $2
+comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-nix-store.sh) | "$1" push -j8 "$2"
